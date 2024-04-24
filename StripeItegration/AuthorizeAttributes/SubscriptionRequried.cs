3dbbefba51e19caaa115235bd13bd1a5d79e79f9
NazarHR <47197105+NazarHR@users.noreply.github.com>
2024-04-24 13:05:41 +0300

    Subscription Access Rework
    changed subscription level comparation logic

--- StripeItegration/AuthorizeAttributes/SubscriptionRequried.cs
+++ StripeItegration/AuthorizeAttributes/SubscriptionRequried.cs
@@ -8,10 +8,10 @@ using System.Web.Mvc.Filters;
 
 namespace StripeItegration.AuthorizeAttributes
 {
-    
+
     public class SubscriptionRequriedAttribute : Attribute, IAuthorizationFilter
     {
-        public string? Level { get; set; } = "Base";
+        public string Level { get; set; } = "Base";
 
         public void OnAuthorization(AuthorizationFilterContext context)
         {
@@ -22,16 +22,26 @@ namespace StripeItegration.AuthorizeAttributes
                 return;
             }
             var identity = (ClaimsIdentity)user.Identity;
-            var currentLevel = identity.FindFirst("SubscriptionLevel").Value;
-            if(currentLevel == null) 
-            { 
-                context.Result = new UnauthorizedResult();
-                return;
+            var currentLevelClaim = identity.FindFirst("SubscriptionLevel");
+            var currentLevelString = "None";
+            if (currentLevelClaim != null)
+            {
+                currentLevelString = currentLevelClaim.Value;
             }
-            if(currentLevel != Level)
+
+            var currentLevel = Enum.Parse<SubscriptionLevel>(currentLevelString);
+            var expectedLevel = Enum.Parse<SubscriptionLevel>(Level);
+            if(currentLevel < expectedLevel)
             {
                 context.Result = new UnauthorizedResult();
             }
         }
+    }
+    enum SubscriptionLevel
+    {
+        None = 0,
+        Base = 1,
+        Platinum = 2,
+        Diamond = 3
     }
 }
